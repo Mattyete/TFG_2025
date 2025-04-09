3f931805d871ec98e50767e2abf4367a493777de
Mattyete <90678148+Mattyete@users.noreply.github.com>
2025-04-09 15:42:26 +0200

    Update setup.py

--- ROS2/ros2_ws/src/limo_vision/setup.py
+++ ROS2/ros2_ws/src/limo_vision/setup.py
@@ -5,19 +5,20 @@ package_name = 'limo_vision'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=['scripts'],
     data_files=[
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/keras_detector.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Matt',
     maintainer_email='mayeteco7@gmail.com',
-    description='Vision with Keras for LIMO in ROS2',
+    description='Vision system using Keras for LIMO robot (ROS 2)',
     license='MIT',
     entry_points={
         'console_scripts': [
-            'keras_detector_v2 = limo_vision.keras_detector_v2:main',
+            'keras_detector_v2 = scripts.keras_detector_v2:main',
         ],
     },
 )
